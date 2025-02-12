c3dc2d103e3562ef10a1eea242532b0f228f6dcb
offkrse <offklaffk@gmail.com>
2025-02-12 16:39:11 +0400

    Create script3.js

---
+++ script3.js
@@ -0,0 +1,17 @@
+const main = function () {
+  console.log('.0');
+  //const visualJSON = JSON.stringify(Object.fromEntries(new URLSearchParams(window.location.search)));
+  //document.querySelector('.param').textContent = visualJSON;
+  
+  console.log('Telegram script started...');
+
+  const unsafeData = window?.Telegram?.WebApp?.initDataUnsafe;
+  const startParam = unsafeData?.start_param;
+
+  const currentParams = new URLSearchParams(window.location.search);
+  if (startParam && currentParams.get('utm_medium') !== startParam) {
+    window.location.search = `utm_medium=${startParam}`;
+    console.log('.1');
+  }
+}
+main();
